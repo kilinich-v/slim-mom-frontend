14b75a75edd5f17658f65aa9ca8a4ee6f064662a
RomanStepanyan <stepik10@gmail.com>
2021-07-28 22:23:19 +0300

    Redux and Persist are added

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import store from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './redux/store'
 import './index.scss'
 import App from './App'
 
 ReactDOM.render(
-  <Provider store={store.store}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root'),
 )
