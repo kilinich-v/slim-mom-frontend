3faec13b09796725c08afcb6d8ae349eed16d57b
Marina Permiakova <63551081+Nickmarina@users.noreply.github.com>
2021-07-29 16:47:54 +0300

    Fixed button styles & added Loader reducer for Modal

--- src/components/common/MainButton/index.jsx
+++ src/components/common/MainButton/index.jsx
@@ -2,7 +2,7 @@ import Button from '@material-ui/core/Button';
 import { orange } from '@material-ui/core/colors';
 import { withStyles } from '@material-ui/core/styles'
 
-const MainButton = withStyles(theme => ({
+const MainButton = withStyles({
     root: {
       borderRadius: '30px',
       width: '176px',
@@ -10,16 +10,18 @@ const MainButton = withStyles(theme => ({
       color: 'white',
       backgroundColor: '#FC842D',
       boxShadow: '0px 4px 10px rgba(252, 132, 45, 0.5)',
+      fontFamily: 'Verdana',
+      fontSize: '14px',
+      lineHeight: '17px',
+      letterSpacing: '0.04em',
+      fontWeight: '700',
+      textAlign: 'center',
+      textTransform: 'capitalize',
+
       '&:hover': {
         backgroundColor: orange[900],
       },
-      [theme.breakpoints.down('768')]: {
-        bottom: '0',
-      },
-      [theme.breakpoints.down('1200')]: {
-        left: '0',
-      },
-    },
-  }))(Button);
+
+  }})(Button);
 
   export default MainButton;
