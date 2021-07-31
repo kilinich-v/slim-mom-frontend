65c87931be0a05ea69388f6c7bee5a0e3057b8c9
Vladimir Kilinich <kylynych.v@gmail.com>
2021-07-31 20:51:17 +0300

    add key to MainModal product

--- src/redux/registration/AxiosInstance.js
+++ src/redux/registration/AxiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  // baseURL: 'https://connections-api.herokuapp.com',
-  baseURL: 'http://localhost:3001/api',
+  baseURL: 'https://connections-api.herokuapp.com',
+  // baseURL: 'http://localhost:3001/api',
 })
 axiosInstance.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
