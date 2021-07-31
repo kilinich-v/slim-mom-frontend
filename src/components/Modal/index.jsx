99484c9616ac567e237c6acb86a5c8b9380918ce
Marina Permiakova <63551081+Nickmarina@users.noreply.github.com>
2021-07-31 18:07:17 +0300

    Fixed modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import {useState, useEffect} from 'react';
+import {useSelector} from 'react-redux';
+import {NavLink} from 'react-router-dom';
 import Modal from '@material-ui/core/Modal';
-import Loader from '../Loader'
-import MainButton from '../common/MainButton';
 import CloseIcon from '@material-ui/icons/Close';
 import KeyboardReturnIcon from '@material-ui/icons/KeyboardReturn';
+import Loader from '../common/Loader'
+import MainButton from '../common/MainButton';
 import {getKcalLoading} from '../../redux/loader/loader-selectors';
+import {getKcalAmount, getProducts} from '../../redux/calculator/calculator-selectors';
+import {getIsLoggedOn} from '../../redux/registration/Selectors';
+import routes from '../../routes'
 import styles from './styles.module.scss';
 
 const MainModal = () => {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   const isLoading = useSelector(getKcalLoading);
-  // const amount = useSelector(getKcalAmount);
-  // const products = useSelector(getProducts)
+  const amount = useSelector(getKcalAmount);
+  const products = useSelector(getProducts);
+  const isLogged = useSelector(getIsLoggedOn);
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
+  useEffect(()=>{
+    console.log(amount)
+    if(amount!==null){
+      setOpen(true);
+    }
+  }, [amount])
+ 
   const handleClose = () => {
     setOpen(false);
   };
@@ -25,44 +33,43 @@ const MainModal = () => {
 
   const modalBody = (
     <div className={styles.modalContainer}>
-      {/* <button className="modalContainer_closeBtn" type="button" onClick={handleClose}> 
-        {document.documentElement.clientWidth > 767 ? <CloseIcon/> : <KeyboardReturnIcon/>}
-      </button> */}
-
       <button className={styles.modalContainer_closeBtn} type="button" onClick={handleClose}> 
         <CloseIcon/>
       </button>
-
       <button className={styles.modalContainer_backBtn} type="button" onClick={handleClose}> 
-        <KeyboardReturnIcon style={{ fontSize: 'inherit', paddingTop: 17, paddingBottom: 16, paddingLeft:15}}/>
+        <KeyboardReturnIcon style={{ fontSize: 'inherit', marginLeft: 15}}/>
       </button>
 
       <h3 className={styles.modalContainer_title}> Ваша рекомендуемая суточная норма калорий составляет </h3>
       <div className={styles.modalContainer_info}>
-          <p className={styles.modalContainer_amount}>- <span className={styles.modalContainer_amount_word}>ккал</span> </p>
+          <p className={styles.modalContainer_amount}>{amount} <span className={styles.modalContainer_amount_word}>ккал</span> </p>
           <h4 className={styles.modalList_title}>Продукты, которые вам <br/> не рекомендуется употреблять</h4>
           <ol className={styles.modalList}>
-              {/* {products.map(product => (
-                <li>{{product}}</li>
-              ))} */}
+              {products&&products.map(product => (
+                <li>{product}</li>
+              ))}
           </ol>
       </div>
-    <MainButton type="button">Начать &#160; <span className={styles.btn_word}>худеть</span></MainButton>
+      <NavLink to={isLogged? routes.diary :routes.reg} style={{textDecoration: 'none'}}> 
+        <MainButton type="button">
+          Начать &#160; <span className={styles.btn_word}>худеть</span>
+        </MainButton>
+    </NavLink>
 </div>
   );
 
   return (
     <div>
-      <MainButton id={styles.openModal_btn} type="submit" onClick={handleOpen}>Похудеть</MainButton>
+      <MainButton id={styles.openModal_btn} type="submit">Похудеть</MainButton>
+      {isLoading? <Loader/> :  
       <Modal
         onClose={handleClose}
         aria-labelledby="modal-title"
         aria-describedby="modal-description"
         open={open}
       >
-        {isLoading ? <Loader/> :  modalBody}
-        {/* {modalBody} */}
-      </Modal>
+        {modalBody}
+      </Modal>}
     </div>
   );
 }
