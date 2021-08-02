0b6e1c5b6804c201276428325fd063d592c5f9ed
RomanStepanyan <stepik10@gmail.com>
2021-08-02 10:42:32 +0300

    Privat and publik routes are added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Switch } from 'react-router-dom'
+
 import routes from './routes'
 import MainPageView from './views/MainPageView'
 import AuthView from './views/AuthView'
@@ -6,17 +7,38 @@ import RegistrationView from './views/RegistrationView'
 import CalculatorView from './views/CalculatorView'
 import DiaryView from './views/DiaryView'
 
+import PublicRoute from './components/Routes/publicRoute'
+import PrivatRoute from './components/Routes/privatRoute'
+
 function App() {
+  пше
   return (
-    <>
-      <Switch>
-        <Route exact path={routes.main} component={MainPageView} />
+    <Switch>
+      {/* My routes*/}
+
+      <PublicRoute exact path={routes.main}>
+        <MainPageView />
+      </PublicRoute>
+      <PublicRoute path={routes.auth} restricted>
+        <AuthView />
+      </PublicRoute>
+      <PublicRoute path={routes.reg} restricted>
+        <RegistrationView />
+      </PublicRoute>
+      <PrivatRoute path={routes.calculator}>
+        <CalculatorView />
+      </PrivatRoute>
+      <PrivatRoute path={routes.diary}>
+        <DiaryView />
+      </PrivatRoute>
+
+      {/* The first routes */}
+      {/* <Route exact path={routes.main} component={MainPageView} />
         <Route path={routes.auth} component={AuthView} />
         <Route path={routes.reg} component={RegistrationView} />
         <Route path={routes.calculator} component={CalculatorView} />
-        <Route path={routes.diary} component={DiaryView} />
-      </Switch>
-    </>
+        <Route path={routes.diary} component={DiaryView} /> */}
+    </Switch>
   )
 }
 
