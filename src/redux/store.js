94def32973d2bb9afec7f12e01f3765a6adabd89
Vladimir Kilinich <kylynych.v@gmail.com>
2021-08-03 12:51:17 +0300

    fix store

--- src/redux/store.js
+++ src/redux/store.js
@@ -15,15 +15,16 @@ import {
 } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import logger from 'redux-logger'
+
 import calcData from './calculator/calculator-reducer'
 import userReducer from './registration/UserSlice'
 import productReducer from './product/product-redusers'
+// import axios from 'axios'
 
-const persistConfig = {
-  key: 'token',
-  storage,
-  whitelist: ['token'],
-}
+// const token = localStorage.getItem('token')
+// const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMDA3YTI0M2VkNDFkMjg4YzljYmZkOCIsImlhdCI6MTYyNzgyNzUxMCwiZXhwIjoxNjI4MTg3NTEwfQ.DXLF17Nku5_flCwPQmbRGOnS-zMQe53u02GZdpKOyi8`
+// axios.defaults.baseURL = 'https://slim-mom-backend.herokuapp.com'
+// axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
 
 const middleware = [
   ...getDefaultMiddleware({
@@ -33,21 +34,22 @@ const middleware = [
   }),
   logger,
 ]
+const persistConfig = {
+  key: 'user',
+  storage,
+  whitelist: ['token'],
+}
 
-// const rootReducer = combineReducers({
-//   kcal: calcData,
-//   user: userReducer,
-//   products: productReducer,
-// })
+const rootReducer = combineReducers({
+  kcal: calcData,
+  user: userReducer,
+  products: productReducer,
+})
 
-const persistedReducer = persistReducer(persistConfig, userReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    kcal: calcData,
-    user: persistedReducer,
-    products: productReducer,
-  },
+  reducer: persistedReducer,
   middleware,
 })
 
